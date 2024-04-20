297faf89d3892f17c1364b8b2f72590c60b0036f
javier-anaya <fidel_k8t@hotmail.com>
2024-04-19 23:12:14 -0600

    remove unused imports

--- src/main/java/com/materiales/bascula/controller/UserController.java
+++ src/main/java/com/materiales/bascula/controller/UserController.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
@@ -35,7 +34,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @Controller
